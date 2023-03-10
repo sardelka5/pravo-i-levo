7c57051a00e9c060a3d4c70cc2c65c31f611f4df
Pavel <pasha311115@yandex.ru>
2023-03-10 15:00:34 +0300

    frontend kawyer

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,16 +4,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App/App';
+import storeLaw from './storeLaw';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   // <React.StrictMode>
-  // <Provider store={store}>
+   <Provider store={storeLaw}>
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  // </Provider>,
+  </Provider>,
   // </React.StrictMode>,
 );

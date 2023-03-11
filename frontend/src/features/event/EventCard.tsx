4829371086341f0ddbfe66f66c365c50b2cf39ef
Pavel <zabolotnikovpg2@mail.ru>
2023-03-11 11:43:00 +0300

    AddMap

--- frontend/src/features/event/EventCard.tsx
+++ frontend/src/features/event/EventCard.tsx
@@ -1,34 +1,63 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
+import {
+  YMaps,
+  Map,
+  Placemark,
+  FullscreenControl,
+  ZoomControl,
+} from 'react-yandex-maps';
+import { Container, Button } from 'react-bootstrap';
 import { RootState } from '../../store';
 
 function EventCard(): JSX.Element {
   const nav = useNavigate();
   const id = useParams();
+  
+  const [coordinates, setCoordinates] = useState<[number, number]>([0, 0]);
 
   const { events } = useSelector((store: RootState) => store.eventState);
+  const event = events.find((el)=> el.id===Number(id.id))
+
+ 
+  const geocode= (ymaps: any):void => {
+    ymaps.geocode(coordinates)
+      .then((result: any) => setCoordinates(result.geoObjects.get(0).geometry.getCoordinates()))
+    }
+
 
   return (
-    <div className="stockContainer">
-      {events
-        .filter((event) => event.id === Number(id.id))
-        .map((event) => (
-          <div key={event.id}>
-            <h1>{event.title}</h1>
-            <div className="imgDesc">
-              <img src={event.photo} alt="eventPhoto" />
-              <div className="evenDate">{event.date}</div>
-              <div className="evenAdress">{event.address}</div>
-              <div className="eventInfo">{event.description}</div>
-            </div>
-          </div>
-        ))}
-      <button type="button" className="btnBack" onClick={() => nav(-1)}>
+    <Container className="stockContainer">
+       <Container key={event?.id}>
+            <h1>{event?.title}</h1>
+            <Container className="imgDesc">
+              <img src={event?.photo} alt="eventPhoto" />
+              <p className="evenDate">{event?.date}</p>
+              <p className="evenAdress">{event?.address}</p>
+              <p className="eventInfo">{event?.description}</p>
+            </Container>
+          <Container className="map">
+            <YMaps onApiAvaliable={geocode}>
+                <Map
+                  defaultState={{
+                  center: coordinates,
+                  zoom: 17,
+                  }}
+                >
+                  <Placemark geometry={coordinates} />
+                  <FullscreenControl options={{ float: 'left' }} />
+                  <ZoomControl options={{ float: 'left' }} />
+               </Map>
+            </YMaps>
+        </Container>
+      </Container>
+
+      <Button type="button" className="btnBack" onClick={() => nav(-1)}>
         Назад
-      </button>
-    </div>
+      </Button>
+    </Container>
   );
 }
 
